614287f841cb242b7abed43349a7447d656849e9
Tyler Hardy <tylerthardy@gmail.com>
2019-09-04 18:27:09 -0500

    Remove zombie code

--- data-model/osrsbox-db/OsrsboxDbObjectType.js
+++ data-model/osrsbox-db/OsrsboxDbObjectType.js
@@ -26,31 +26,3 @@ module.exports = class OsrsboxDbObjectType extends GraphQLObjectType {
         });
     }
 }
-
-
-
-/*
-
-let obj = {};
-
-function OsrsDbObjectType(obj) {
-    this.obj = obj;
-}
-
-function constructGraphQLObject () {
-    let graphQLObject = {};
-
-    for (const [key, value] of Object.entries(this.obj)) {
-        graphQLObject = new GraphQLFieldConfig(
-            { type: value, resolve: (json) => json[key] }
-        )
-    }
-}
-
-module.exports = new OsrsDbObjectType({
-    name: "Item",
-    description: "An ItemDefinition object type includes basic item metadata such as id, name, examine text, store cost, high_alch and low_alch values and quest_item association. Every item object in the item database. All of the properties available are listed in the table below including the property name, the data types used, a description of the property and if the property is required to be populated - if not required, the property value can potentially be set to None.",
-
-    fields: () => (constructGraphQLObject())
-});
-*/
